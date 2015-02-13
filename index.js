29e85f4579deaa0d6aa189c0b6dff6ddcd9416ec
SnareChops <snare_chops@yahoo.com>
2015-02-13 01:14:17 -0500

    Added hit counter

---
+++ index.js
@@ -0,0 +1,28 @@
+/*globals Parse:false, angular:false*/
+angular.module('blog', [])
+.config(['$interpolateProvider', function($interpolateProvider){
+  $interpolateProvider.startSymbol('[!').endSymbol('!]');
+  }])
+  .controller('PostController', ['$http', function($http){
+    var self = this;
+
+    self.pageInit = function(pageid){
+      self.pageid = encodeURIComponent(pageid);
+
+      $http.get('http://videos.quarrantine.com:8080/hit?id='+encodeURIComponent(self.pageid)).then(function(data){
+        self.hits = data.data;
+      });
+
+      //$http.get('http://videos.quarrantine.com:8080/likes?id='+encodeURIComponent(self.pageid)).then(function(data){
+      //  self.incs = data.data.likes;
+      //  self.decs = data.data.dislikes;
+      //});
+    };
+
+    //self.like = function(inc){
+    //  $http.get('http://videos.quarrantine.com:8080/likes?id='+encodeURIComponent(self.pageid)+'&inc='+inc.toString()).then(function(data){
+    //    self.incs = data.data.likes;
+    //    self.decs = data.data.dislikes;
+    //  });
+    //};
+  }]);
