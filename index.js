d797a35be72af123a6cb0a8a4ac0152f5038a584
Scott Robinson <snare_chops@yahoo.com>
2015-02-15 17:16:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ angular.module('blog', [])
     //  });
     //};
   }]);
-new_key: function() {
+var new_key = function() {
   function _p8(s) {
     p = (Math.random().toString(16) + "000000000").substr(2, 8);
     return s ? "-" + p.substr(0, 4) + "-" + p.substr(4, 4) : p;
