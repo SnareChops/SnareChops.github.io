07cf14db386dc7476a9a0de9450d580de69355d8
SnareChops <snare_chops@yahoo.com>
2015-03-17 00:17:22 -0400

    Adding new endpoints

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 /*globals Parse:false, angular:false*/
 angular.module('blog', [])
 .run(function(){OAuth.initialize('7m3XJ8bHoZV6MN2Hvfr5W5-_5rw')})
+.constant('BlogAPIUrl', 'http://videos.quarrantine.com:8080') //Production
+//.constant('BlogAPIUrl', 'http://pink-puffy-poodle-197724.use1-2.nitrousbox.com') //Development
 .config(['$interpolateProvider', '$sceDelegateProvider', function($interpolateProvider, $sceDelegateProvider){
   $interpolateProvider.startSymbol('[!').endSymbol('!]');
   $sceDelegateProvider.resourceUrlWhitelist(['self', 'http://videos.quarrantine.com:8080/**', 'http://pink-puffy-poodle-197724.use1-2.nitrousbox.com/**']);
 }])
-.controller('PostController', ['$http', function($http){
+.controller('PostController', ['$http', 'BlogAPIUrl', function($http, BlogAPIUrl){
   var self = this;
     self.views = {};
     self.pageid = null;
@@ -16,20 +18,21 @@ angular.module('blog', [])
     self.comments = [];
 
     self.indexInit = function(){
-      $http.get('http://videos.quarrantine.com:8080/hits.json').then(function(data){
+      $http.get(BlogAPIUrl+'/hits.json').then(function(data){
         data.data.forEach(function(x){
           self.views[x.postid] = x.count;
         });
       });
     };
 
-    self.pageInit = function(pageid){
+    self.pageInit = function(pageid, postname){
       self.pageid = encodeURIComponent(pageid);
-      $http.get('http://videos.quarrantine.com:8080/comments/'+pageid+'.json').then(function(data){
+      self.postname = postname;
+      $http.get(BlogAPIUrl+'/comments/'+pageid+'.json').then(function(data){
         self.comments = data.data;
       });
       if(self.user == null || self.user.username !== 'SnareChops') {
-        $http.put('http://videos.quarrantine.com:8080/hit/' + self.pageid).then(function (data) {
+        $http.put(BlogAPIUrl+'/hit/' + self.pageid).then(function (data) {
           self.hits = data.data;
         });
       }
@@ -41,16 +44,16 @@ angular.module('blog', [])
     };
 
     self.subscribe = function(email){
-      $http.post('http://videos.quarrantine.com:8080/subscribe', {email: email}).then(function(data){
+      $http.post(BlogAPIUrl+'/subscribe', {email: email}).then(function(data){
         self.subscribeButtonText = 'Thank you!';
         self.email = '';
       });
     };
 
     self.login = function() {
-      $http.get('http://videos.quarrantine.com:8080/token.json', {withCredentials: true}).then(function (data) {
+      $http.get(BlogAPIUrl+'/token.json', {withCredentials: true}).then(function (data) {
         OAuth.popup('github', {state: data.data}).done(function (result) {
-          $http.post('http://videos.quarrantine.com:8080/authorize.json', {code: result.code}, {withCredentials: true}).then(function (data) {
+          $http.post(BlogAPIUrl+'/authorize.json', {code: result.code}, {withCredentials: true}).then(function (data) {
             localStorage.setItem('user', JSON.stringify({
               email: data.data.email,
               image: data.data.image,
@@ -64,14 +67,22 @@ angular.module('blog', [])
       });
     };
 
-      self.makeComment = function(comment){
-        $http.post('http://videos.quarrantine.com:8080/comments.json', {postid: self.pageid, comment: comment}, {withCredentials:true}).then(function(data){
-          self.comment = null;
-          self.comments.push(data.data);
-        }, function(){
-          alert('Error making comment. Please be patient while I am working on this new feature.');
-        });
-      };
+    self.makeComment = function(comment){
+      $http.post(BlogAPIUrl+'/comments.json', {postid: self.pageid, comment: comment, postname: self.postname}, {withCredentials:true}).then(function(data){
+        self.comment = null;
+        self.comments.push(data.data);
+      }, function(){
+        alert('Error making comment. Please be patient while I am working on this new feature.');
+      });
+    };
+
+    self.pushPost = function(postid, title, description, url, posted_on){
+      $http.post(BlogAPIUrl+'/posts.json', {postid: postid, title: title, description: description, url: url, posted_on: posted_on}, {withCredentials:true}).then(function(data){
+        alert('Post created successfully');
+      }, function(){
+        alert('Error saving post to server.');
+      });
+    };
 
     //self.like = function(inc){
     //  $http.get('http://videos.quarrantine.com:8080/likes?id='+encodeURIComponent(self.pageid)+'&inc='+inc.toString()).then(function(data){
